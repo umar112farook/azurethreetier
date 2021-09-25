f40ef7448479920e6b3fee95e2955a95832281e7
umar112farook <91374265+umar112farook@users.noreply.github.com>
2021-09-25 22:06:12 +0530

    Update vars.tf

--- modules/database/vars.tf
+++ modules/database/vars.tf
@@ -1,1 +1,8 @@
+variable "primary_database" {}
+variable "resource_group" {}
+variable "location" {}
+variable "primary_database_version" {}
+variable "primary_database_admin" {}
+variable "primary_database_password" {}
+
 
